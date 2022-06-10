6a216e83509a9ca3bb3a0772c6b8c667109037f4
Ross Kirkpatrick <ross.kirkpatrick@suse.com>
2022-06-10 07:53:44 -0400

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = 'SilentlyContinue';
+Write-Output "test curl"
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox curl -o docker.zip https://download.docker.com/win/static/edge/x86_64/docker-17.10.0-ce.zip
+
+Write-Output "test http wget"
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox wget http://google.com
+
+Write-Output "test https wget"
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox wget https://google.com
+
+Write-Output "test unzip"
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox unzip docker.zip
+
+Write-Output "test ls unzipped folder"
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox ls docker
+$ErrorActionPreference = 'Stop';
+
+Write-Output "test ls unzipped folder on host"
+ls docker
